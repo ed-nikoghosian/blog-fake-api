b202fb131c29ec02894c34334f85954215990ad9
ed-nikoghosian <ed.nikoghosian@gmail.com>
2023-05-20 15:22:07 +0400

    changed server

--- server.js
+++ server.js
@@ -1,22 +1,45 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('./db.json');
 
+// const middlewares = jsonServer.defaults();
+
+// server.use(middlewares);
+
+// server.use(
+//   jsonServer.rewriter({
+//     '/api/*': '/$1'
+//   })
+// );
+
+// server.use(router);
+
+// const port = process.env.PORT || 4000;
+// server.listen(port, () => {
+//   console.log('JSON Server is running on port', port);
+// });
+
+// module.exports = server;
+const jsonServer = require('json-server');
+const clone = require('clone');
+const data = require('./db.json');
+const server = jsonServer.create();
+
+const router = jsonServer.router(clone(data), {
+  _isFake: true
+});
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 
-server.use(
-  jsonServer.rewriter({
-    '/api/*': '/$1'
-  })
-);
+server.use((req, res, next) => {
+  if (req.path !== '/') router.db.setState(clone(data));
+  next();
+});
 
 server.use(router);
-
-const port = process.env.PORT || 4000;
-server.listen(port, () => {
-  console.log('JSON Server is running on port', port);
+server.listen(process.env.PORT || 8000, () => {
+  console.log('JSON Server is running');
 });
 
 module.exports = server;
